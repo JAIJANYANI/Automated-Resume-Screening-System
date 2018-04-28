6c41ac117e1923c73a75b78c22430766209bb4b4
Jai Janyani <jaijanyani@gmail.com>
2018-04-28 07:01:52 +0530

    new

--- Main_1.py
+++ Main_1.py
@@ -40,13 +40,15 @@ Tested on Ubuntu 16.04 LTS amd64 xenial image built on 2017-09-19
             # python Main_1.py
 
 NOTE: This API runs on "127.0.0.1/results" URL
+        pip install git+git://github.com/deanmalmgren/textract.git@master
+        https://www.lfd.uci.edu/~gohlke/pythonlibs/#pocketsphinx
 
 """
 
 import glob
 import os
 import warnings
-
+import textract
 import requests
 from flask import (Flask, json, jsonify, redirect, render_template, request,
                    url_for)
@@ -56,6 +58,7 @@ from sklearn.neighbors import NearestNeighbors
 from werkzeug import secure_filename
 
 import pdf2txt as pdf
+import PyPDF2
 
 warnings.filterwarnings(action='ignore', category=UserWarning, module='gensim')
 
@@ -79,15 +82,50 @@ def resume():
     Resume_Vector = []
     Ordered_list_Resume = []
     Ordered_list_Resume_Score = []
-    LIST_OF_PDF_FILES = []
-    os.chdir('A:\Forsk\Resume_Parser\Flaskify\Original_Resumes')
-    for file in glob.glob("*.pdf"):
-        LIST_OF_PDF_FILES.append(file)
+    LIST_OF_FILES = []
+    Resumes = []
+    Temp_pdf = []
+    os.chdir('./Original_Resumes')
+    for file in glob.glob("*"):
+        LIST_OF_FILES.append(file)
+    LIST_OF_FILES.remove("antiword.exe")
+    print("This is LIST OF FILES")
+    print(LIST_OF_FILES)
 
-    print("Total Files to Parse\t" , len(LIST_OF_PDF_FILES))
+    # print("Total Files to Parse\t" , len(LIST_OF_PDF_FILES))
     print("####### PARSING ########")
-    for i in LIST_OF_PDF_FILES:
-        pdf.extract_text([str(i)] , all_texts=None , output_type='text' , outfile='../Parsed_Resumes/'+str(i)+'.txt')
+    for nooo,i in enumerate(LIST_OF_FILES):
+        Ordered_list_Resume.append(i)
+        Temp = i.split(".")
+        if Temp[1] == "pdf" or Temp[1] == "Pdf" or Temp[1] == "PDF":
+            print("This is PDF" , nooo)
+            with open(i,'rb') as pdf_file:
+                read_pdf = PyPDF2.PdfFileReader(pdf_file)
+                page = read_pdf.getPage(0)
+                page_content = page.extractText()
+                Resumes.append(Temp_pdf)
+
+                # number_of_pages = read_pdf.getNumPages()
+                # for page_number in range(number_of_pages): 
+                #     page = read_pdf.getPage(page_number)
+                #     page_content = page.extractText()
+                #     Temp_pdf.append(page_content)
+                # Resumes.append(Temp_pdf)
+        if Temp[1] == "doc" or Temp[1] == "Doc" or Temp[1] == "DOC":
+            print("This is DOC" , i)
+            
+            Resumes.append(textract.process(i))
+        if Temp[1] == "docx" or Temp[1] == "Docx" or Temp[1] == "DOCX":
+            print("This is DOCX" , i)
+            Resumes.append(textract.process(i))
+        if Temp[1] == "ex" or Temp[1] == "Exe" or Temp[1] == "EXE":
+            print("This is EXE" , i)
+            pass
+
+
+
+    # print(Resumes)
+        # pdf.extract_text([str(i)] , all_texts=None , output_type='text' , outfile='../Parsed_Resumes/'+str(i)+'.txt')
 
     print("Done Parsing.")
 
@@ -116,16 +154,20 @@ def resume():
         print("\n\n")
 
 
-    LIST_OF_TXT_FILES = []
-    os.chdir('../Parsed_Resumes')
-    for file in glob.glob("*.txt"):
-        LIST_OF_TXT_FILES.append(file)
+    # LIST_OF_TXT_FILES = []
+    # os.chdir('../Parsed_Resumes')
+    # for file in glob.glob("*.txt"):
+    #     LIST_OF_TXT_FILES.append(file)
 
-    for i in LIST_OF_TXT_FILES:
-        Ordered_list_Resume.append(i)
-        f = open(i , 'r')
-        text = f.read()
+    for i in Resumes:
         
+        # f = open(i , 'r')
+        # print("###########################################################################################")
+        # print("###########################################################################################")
+        # print(i)
+        # print("###########################################################################################")
+        # print("###########################################################################################")
+        text = i
         tttt = str(text)
         tttt = summarize(tttt, word_count=100)
         # print(tttt) ## Summarized Text
@@ -173,7 +215,7 @@ def resume():
     print(Ordered_list_Resume_Score)
     flask_return = []
     for n,i in enumerate(Z):
-        print("Rank\t" , n+1, ":\t" , i)
+        print("Rankkkkk\t" , n+1, ":\t" , i)
         # flask_return.append(str("Rank\t" , n+1, ":\t" , i))
 
     return str(Z)
